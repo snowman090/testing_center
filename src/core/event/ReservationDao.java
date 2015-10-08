5d308703a1b0b0248d92db19d139a99aee7eca2c
yimin <zym19930828@gmail.com>
2015-10-09 04:53:26 +0800

    Merge branch 'master' of C:\Users\Acer\IdeaProjects\testing_center with conflicts.

--- src/core/event/ReservationDao.java
+++ src/core/event/ReservationDao.java
@@ -1,12 +1,13 @@
 package core.event;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface ResrvationDao {
+public interface ReservationDao {
     List<Reservation> findAll();
-    List findByID();
-    List<Reservation> findByDate();
-    List<Reservation> findByInstructor();
+    List findByID(String reservationID);
+    List<Reservation> findByDate(LocalDate date);
+    List<Reservation> findByInstructor(String InstructorID);
     boolean insertReservation(Reservation reservation);
     boolean updateReservation(Reservation reservation);
     boolean deleteReservation(Reservation reservation);
