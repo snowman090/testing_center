a8d9833d901a2dc5742624f9d0d374f9b2cd0413
walt wu <wentao.wu@stonybrook.edu>
2015-10-07 22:52:31 -0400

    sd

--- src/core/Course.java
+++ src/core/Course.java
@@ -1,11 +1,41 @@
 package core;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Course {
     @Id
+    @Column(name = "COURSE_NAME")
     private String courseId;
+    @Basic(optional = false)
+    private Instructor instructor;
+    @Basic(optional = false)
+    @Column(name = "ENROLLED_STUDENTS")
+    @OneToMany
+    private List<Student> enrolledStudents;
 
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(List<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
 }
