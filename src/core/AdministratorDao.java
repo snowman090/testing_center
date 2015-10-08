1b65e1a6e19f4b9bd871013928ae6cea62897c8e
zeqli <l.caecar@gmail.com>
2015-10-07 21:33:00 -0400

    From Zeqli

--- src/core/AdministratorDao.java
+++ src/core/AdministratorDao.java
@@ -1,5 +1,33 @@
 package core;
 
+
 public interface AdministratorDao {
+
+
+    // Import data.
+    public void importData();
+
+    // Import User.csv
+    public boolean importUserCSV();
+
+    // Import Class.csv
+    public boolean importClassCSV();
+
+    // Import Roster.csv
+    public boolean importRoster();
+
+    // Display Utilization
+    public void displayUtilization();
+
+    // Handle schedule request
+    public void handleScheduleRequest();
+
+    // Make appointment on behalf of a student
+    public void makeApptOnBehalfOfStudent();
+
+    // View Appointment
+    public void viewAppointment();
+    public void checkInStudent();
+    public void generateReport();
 
 }
