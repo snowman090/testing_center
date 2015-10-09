9963c141caaed3d38240ac6c85f90c87f397aa73
yimin <zym19930828@gmail.com>
2015-10-09 12:30:19 +0800

    readCVSfile_fix

--- src/core/event/DataCollection.java
+++ src/core/event/DataCollection.java
@@ -6,10 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class DataCollection {
     static final private String PATH = "/course_registration_data/";
@@ -28,122 +25,7 @@ public class DataCollection {
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-        list = new List<String[]>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<String[]> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] ts) {
-                return null;
-            }
-
-            @Override
-            public boolean add(String[] strings) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends String[]> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int i, Collection<? extends String[]> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public String[] get(int i) {
-                return new String[0];
-            }
-
-            @Override
-            public String[] set(int i, String[] strings) {
-                return new String[0];
-            }
-
-            @Override
-            public void add(int i, String[] strings) {
-
-            }
-
-            @Override
-            public String[] remove(int i) {
-                return new String[0];
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<String[]> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<String[]> listIterator(int i) {
-                return null;
-            }
-
-            @Override
-            public List<String[]> subList(int i, int i1) {
-                return null;
-            }
-        };
+        list = new ArrayList<String[]>();
         try {
             br = new BufferedReader(new FileReader(path));
             while ((line = br.readLine()) != null) {
