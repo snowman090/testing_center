b26e8a62dc465dd0c4e6604602ab5bba606ffd17
snowman090 <walt090331@gmail.com>
2015-10-23 16:29:38 -0400

    controller started

--- src/core/controller/LoginController.java
+++ src/core/controller/LoginController.java
@@ -6,18 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController{
-
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginForm (Model model) {
-        model.addAttribute("login", new Login());
-        return "login";
-    }
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginSubmit (@ModelAttribute Login login, Model model) {
-        model.addAttribute("login", login);
-        return "login";
+    public ModelAndView loginSubmit (@RequestParam("netId") String userId, @RequestParam("password") String password) {
+        ModelAndView model = new ModelAndView("home");
+        return model;
     }
 }
--- src/core/user/StudentDao.java
+++ src/core/user/StudentDao.java
@@ -1,7 +1,10 @@
 package core.user;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface StudentDao {
     List<Student> getAllStudents ();
     List<Student> getStudentsByName (String firstName, String lastName);
