80f8ba70be7341ee655f9216a9b3d2475054f9bf
snowman090 <walt090331@gmail.com>
2015-10-24 02:21:09 -0400

    login controller *almost* completed, no form validation needed

--- src/core/controller/LoginController.java
+++ src/core/controller/LoginController.java
@@ -1,12 +1,16 @@
 package core.controller;
 
 import core.user.AuthenticationService;
+import core.user.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController{
@@ -17,7 +21,38 @@ public class LoginController{
     public ModelAndView loginSubmit (@RequestParam("netId") String userId,
                                      @RequestParam("password") String password) {
         ModelAndView model = new ModelAndView();
-        if (authenticationService.login(userId, password) != null) {
+        Authorization authorization = authenticationService.login(userId, password);
+        if (authorization != null) {
+            //code here to determine user permission
+            Map<String, Object> viewVariables = new HashMap<>();
+            /*
+            here i feel the need to make changes to the data structure for each different
+            type of user's permission level, instead of using enum type field, which each type of
+            user has only one, making it a knot to allow the user to switch roles without logging out
+            and hogging up empty spaces in the db tables. An alternative may be to use numbers to
+            represent permission levels, and we can determine if the user has multiple identities
+            mathematically.
+            */
+            switch (authorization) {
+                case ADMINISTRATOR:
+                    viewVariables.put("user-level", Messages.USER_ADMINISTRATOR);
+                    viewVariables.put("operations", Messages.ADMINISTRATOR_OPERATIONS);
+                    /*...*/
+                    break;
+                case STUDENT:
+                    viewVariables.put("user-level", Messages.USER_STUDENT);
+                    viewVariables.put("operations", Messages.STUDENT_OPERATIONS);
+                    /*...*/
+                    break;
+                case INSTRUCTOR:
+                    viewVariables.put("user-level", Messages.USER_INSTRUCTOR);
+                    viewVariables.put("operations", Messages.INSTRUCTOR_OPERATIONS);
+                    /*...*/
+                    break;
+            }
+            //add all objects to the model
+            model.addAllObjects(viewVariables);
+
             model.setViewName("home");
             return model;
         }else{
--- src/core/user/AuthenticationService.java
+++ src/core/user/AuthenticationService.java
@@ -1,7 +1,21 @@
 package core.user;
 
 public interface AuthenticationService {
+    /**
+     * determine whether the user associated with the userId is
+     * registered in the db
+     * @param userId
+     * @return
+     */
     boolean registeredUserId(String userId);
 
+    /**
+     * determine the user's permission level
+     * @param userId
+     * @param password
+     * @return the permission level of the user associated with the
+     *          userId and password, returns null if password does not
+     *          match the userId
+     */
     Authorization login (String userId, String password);
 }
