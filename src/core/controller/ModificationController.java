58e41bd651946b2451f58ef4c3957d94fdfb5fa6
walt wu <wentao.wu@stonybrook.edu>
2015-10-30 12:00:55 -0400

    home controller modified

--- src/core/controller/ModificationController.java
+++ src/core/controller/ModificationController.java
@@ -3,6 +3,7 @@ package core.controller;
 import core.service.TestingCenterInfoRetrieval;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.portlet.ModelAndView;
@@ -21,13 +22,16 @@ public class ModificationController {
     /**
      * this method is called when a information cell is clicked and
      * the user is then allowed to change the contents of the fields of
-     * testing center information
+     * testing center information. To prompt the user to change the selected
+     * field, a popup window will appear, asking for input for the specified
+     * field.
      * @return
      */
-    @RequestMapping(value = "/edit-info-form", method = RequestMethod.GET)
-    public ModelAndView editTestingCenterInfo (){
-        ModelAndView modelAndView = new ModelAndView();
-        return modelAndView;
+    @RequestMapping(value = "/edit-info/{field-type}/form", method = RequestMethod.GET)
+    public ModelAndView editTestingCenterInfo (@PathVariable("field-type") String fieldType){
+        ModelAndView model = new ModelAndView("edit-info");
+        model.addObject("modify-field", fieldType);
+        return model;
     }
 
     @RequestMapping(value = "/edit-info-submit", method = RequestMethod.POST)
