b1a7a184687579e11f133d4ec47b39e58920c3e7
walt wu <wentao.wu@stonybrook.edu>
2015-11-01 16:39:08 -0500

    mess in exam fixed

---
+++ src/core/event/ExamDaoImp.java
@@ -0,0 +1,45 @@
+package core.event;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ExamDaoImp implements ExamDao{
+    List<Exam> exams;
+    public ExamDaoImp(){
+
+        exams = new ArrayList<Exam>();
+
+    }
+
+    @Override
+    public List<Exam> getAllExams() {
+        return exams;
+    }
+
+    @Override
+    public List<Exam> findByexamId(String examId) {
+        return null;
+    }
+
+
+    @Override
+    public boolean addExam(Exam exam) {// return should have different cases
+        exams.add(exam);
+        return true;
+    }
+
+    @Override
+    public boolean updateExam(Exam exam, String examId) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCExam(String examId) {
+        return false;
+    }
+
+
+}
