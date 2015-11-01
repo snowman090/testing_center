bbef21b0b32d39d58655b9ecaac6815b129f4539
walt wu <wentao.wu@stonybrook.edu>
2015-11-01 17:58:18 -0500

    nav controller modified

--- src/core/controller/AdminNavigationController.java
+++ src/core/controller/AdminNavigationController.java
@@ -5,6 +5,7 @@ import core.event.ReservationDao;
 import core.service.TestingCenterInfoRetrieval;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
 
@@ -17,20 +18,24 @@ public class AdminNavigationController {
     @Autowired
     private AppointmentDao appointmentDao;
 
+    private ModelMap model = new ModelMap();
+
+    private String viewName = "admin-home";
+
     /**
      * this is a controller method for view and edit information
      * this functionality is for administrator only
      * @return
      */
     @RequestMapping("/view-info")
-    public ModelAndView viewCenterInfo() {
-        ModelAndView model = new ModelAndView("edit-info");
+    public String viewCenterInfo() {
+        model.clear();
         //set the heading to that of view information page
-        model.addObject("page_heading",
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("viewInfo"));
         //infoRetrieval method will get a Map of testing center information
-        model.addObject("center_info", infoRetrieval.retrieveInfo());
-        return model;
+        model.addAttribute("main_content", infoRetrieval.retrieveInfo());
+        return viewName;
     }
 
     /**
@@ -39,56 +44,56 @@ public class AdminNavigationController {
      * @return
      */
     @RequestMapping("/upload")
-    public ModelAndView uploadFile() {
-        ModelAndView model = new ModelAndView("upload-file");
-        model.addObject("page_heading",
+    public String uploadFile() {
+        model.clear();
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("uploadFile"));
-        return model;
+        return viewName;
     }
 
     @RequestMapping("/view-requests")
-    public ModelAndView viewRequests() {
-        ModelAndView model = new ModelAndView("view-requests");
-        model.addObject("page_heading",
+    public String viewRequests() {
+        model.clear();
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("viewRequest"));
         /*by default, when the user enters the page all requests are displayed in chronological order
         by selecting different tabs on top of the list, the user is able to view the list
         in different orders: by alphabetical order of instructors' last names, number of attendants,
         utilization, or, display only the ones made by one instructor by search the instructor's name*/
-        model.addObject("all_requests", reservationAccess.findAll());
-        return model;
+        model.addAttribute("main_content", reservationAccess.findAll());
+        return viewName;
     }
 
     @RequestMapping("/view-appointments")
-    public ModelAndView viewAppointments() {
-        ModelAndView model = new ModelAndView("view-appointments");
-        model.addObject("page_heading",
+    public String viewAppointments() {
+        model.clear();
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("viewAppointments"));
-        model.addObject("all_appointments", appointmentDao.findAllAppointment());
-        return model;
+        model.addAttribute("main_content", appointmentDao.findAllAppointment());
+        return viewName;
     }
 
     @RequestMapping("/make-appointment")
-    public ModelAndView makeAppointment() {
-        ModelAndView model = new ModelAndView("make-appointments");
-        model.addObject("page_heading",
+    public String makeAppointment() {
+        model.clear();
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("makeAppointment"));
-        return model;
+        return viewName;
     }
 
     @RequestMapping("/check-in")
-    public ModelAndView checkIn() {
-        ModelAndView model = new ModelAndView("check-in");
-        model.addObject("page_heading",
+    public String checkIn() {
+        model.clear();
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("checkIn"));
-        return model;
+        return viewName;
     }
 
     @RequestMapping("/generate-report")
-    public ModelAndView generateReport() {
-        ModelAndView model = new ModelAndView("general-report");
-        model.addObject("page_heading",
+    public String generateReport() {
+        model.clear();
+        model.addAttribute("page_heading",
                 StringResources.ADMINISTRATOR_OPERATIONS.get("generateReport"));
-        return model;
+        return viewName;
     }
 }
