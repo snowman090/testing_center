11c6d51ee01eb9e6f475cae0f345eac7882f41b4
Zeqli <zeqing.li@stonybrook.edu>
2015-11-02 17:47:05 -0500

    1. Implement Report Year Report Functionality 2. Fill in ExamDao

--- src/core/event/ExamDaoImp.java
+++ src/core/event/ExamDaoImp.java
@@ -1,12 +1,19 @@
 package core.event;
 
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
+import core.service.SessionManager;
+import org.springframework.web.util.Log4jConfigListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ExamDaoImp implements ExamDao{
+    public static final Logger log = Logger.getLogger(ExamDaoImp.class);
     List<Exam> exams;
     public ExamDaoImp() {
 
@@ -26,8 +33,34 @@ public class ExamDaoImp implements ExamDao{
 
     @Override
     public boolean addExam(Exam exam) {// return should have different cases
-        exams.add(exam);
-        return true;
+//        exams.add(exam);
+//        return true;
+// Comment off by Zeqing Li.
+        Session session = SessionManager.getInstance().getOpenSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(exam);
+            log.info("---------- add Exam info----------");
+            log.info("|  -Exam Id: " + exam.getExamId());
+            log.info("|  -Exam Name: " + exam.getExamName());
+            log.info("|  -Type: " + exam.getType());
+            log.info("|  -Start Date Time: " + exam.getStartDateTime());
+            log.info("|  -End Date Time" + exam.getEndDateTime());
+            log.info("|  -Duration: " + exam.getDuration());
+            log.info("|  -Number of Students who has appointments: " + exam.getNumStudentAppointment());
+            log.info("|  -Number of Students who need to take exam: " + exam.getNumStudentNeed());
+
+            tx.commit();
+            session.close();
+        }catch (HibernateException he){
+            if(tx != null){
+                tx.rollback();
+            }
+            log.error("Error with addExam ", he);
+            return false;
+        }
+        return  true;
     }
 
     @Override
